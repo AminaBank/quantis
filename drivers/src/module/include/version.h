aac412b9e731d4690850a0192f6728fe6470472d
Geoff <sanddweller@gmail.com>
2026-04-15 09:54:08 +0400

    UPLOAD2

---
+++ drivers/src/module/include/version.h
@@ -0,0 +1,12 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define DRV_MOD_VERSION "24.4.2"
+
+#define DRV_MOD_MAJOR 24
+#define DRV_MOD_MINOR 4
+#define DRV_MOD_PATCH 2
+
+#define DRV_MOD_VERSION_NUMBER ((DRV_MOD_MAJOR)*10 + DRV_MOD_MINOR)
+
+#endif /* ifndef __VERSION_H__ */
